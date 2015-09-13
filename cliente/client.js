ab92d9914275b135bed32158ad4b36d01ae9bec1
CristiaN <cristiannavarrete@outlook.com>
2015-09-13 12:21:35 -0300

    Preparing V2
    Added Support for DS18B20
    New Design
    *Graphs NOT working.
    *Need to fix the problem with the ram USSAGE over time.

--- cliente/client.js
+++ cliente/client.js
@@ -1,10 +1,14 @@
 // Vars
 var timeout;
-var server = '';
+var server = 'http://127.0.0.1:3700/';
 var timeBetweenUpdates = 60; // Time between auto updates in seconds
 var token = '';
-
 var ports = {'interior': 25, 'exterior': 23};
+var ubicaciones = {
+	'28-0314642bd5ff': 'Piscina',
+	'28-0314640479ff': 'Tinaja'
+};
+
 // For my raspberry the ports are
 //P4 (23) -> Exterior
 //P6 (25) -> Interior
@@ -13,11 +17,55 @@ var ports = {'interior': 25, 'exterior': 23};
 // Requires
 var io = require('socket.io-client')(server);
 var sensorLib = require("node-dht-sensor");
+var ds18b20 = require('ds18b20');
+
 
 /// Get the data
 function getData(pos){
 	var data = sensorLib.readSpec(22, pos);
 	return data
+}
+
+// Water Temperature
+function getSensors(fallback){
+	ds18b20.sensors(function(err, ids) {
+		fallback(ids);
+	});
+}
+
+function getData(id,fallback,key,test){
+	ds18b20.temperature(id, function(err, value) {
+		fallback(value,key,test);
+	});
+}
+function sendData(temperature,ubicacion,test){
+	if(temperature){
+		tosend = {};
+		tosend.token = token;
+		tosend[ubicacion] = temperature.toFixed(2);
+		if(!test){
+			console.log('Sending Data');
+			conn.emit('updateDb', tosend, function(resp, data) {
+				console.log('Respuesta' + resp,data);
+			});
+		}
+		else{
+			console.log('Test Mode',tosend);
+		}
+	}
+}
+function updateData(test){
+	test || ( test = false );
+	getSensors(function(sensors){
+		if(sensors){
+			sensors = sensors;
+			for (var key in sensors){
+				console.log(key);
+				ubicacion = ubicaciones[sensors[key]];
+				getData(sensors[key],sendData,ubicacion,test);
+			}
+		}
+	});
 }
 
 // Handle socket request
@@ -53,6 +101,12 @@ function update(){
 		
 	}
 	timeout = setTimeout(function(){update();},timeBetweenUpdates * 1000);
+	updateData();
 }
+
+updateData();
+setInterval(updateData(),timeBetweenUpdates * 1000);
+
+
 
 console.log('Client Started');
--- servidor/server.js
+++ servidor/server.js
@@ -1,8 +1,10 @@
 // Vars
-var mongoUrl = 'mongodb://user:pass@127.0.0.1:27017/temps' // MongoDB Data
+var mongoUrl = 'mongodb://admin:pass@127.0.0.1:27017/temps'; // MongoDB Data
 var port = 3700; // For the Webserver and Socket Server
 var token = ''; // Same from client.js
 var timeBetweenUpdates = 30; // Minimum time between updates from raspberry (In Seconds)
+var timeBetweenGraph = 60 * 60;
+var timeGetRecords = 24 * 60 * 60 * 1000 * 7;
 var recibidos = [
 	{
 		"sensorName": "Interior",
@@ -23,26 +25,43 @@ var recibidos = [
 		"sensorName": "Exterior",
 		"sensorKind": "humidity",
 		"values": [],
-	}
+	},
+	{
+		"sensorName": "Piscina",
+		"sensorKind": "temperature",
+		"values": [],
+	},
+	{
+		"sensorName": "Tinaja",
+		"sensorKind": "temperature",
+		"values": [],
+	},
 ];
 
 var lastUpdates = {
 	interior: new Date().getTime()-(timeBetweenUpdates+1)*1000,
 	exterior: new Date().getTime()-(timeBetweenUpdates+1)*1000,
-}
+	piscina: new Date().getTime()-(timeBetweenUpdates+1)*1000,
+	tinaja: new Date().getTime()-(timeBetweenUpdates+1)*1000
+};
+var maxmin;
+
 
 //Required
+var moment = require('moment');
 var express = require("express");
 var app = express();
 var io = require('socket.io').listen(app.listen(port));
 var MongoClient = require('mongodb').MongoClient;
-var rootDir =  __dirname + '/views/'
+var bodyParser = require('body-parser');
+var rootDir =  __dirname + '/public/';
 
-// Static dirs for express
-app.use(express.static(__dirname + '/views/'));
-app.use(express.static(__dirname + '/assets/'));
-
-// Helper Functions
+// Static dirs for express and handle post
+app.use(express.static(__dirname + '/public/'));
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); // Helper Functions
 
 var verifyDate = function(initial,toverify,interval){
 	if (toverify-interval > initial){
@@ -51,8 +70,29 @@ var verifyDate = function(initial,toverify,interval){
 	else{
 		return false;
 	}
-}
-
+};
+var getMaxMin = function(sensorName, day){
+	// Get key
+	var max = -100;
+	var min = 100;
+	for (var id in recibidos){
+		if(recibidos[id].sensorName === sensorName && recibidos[id].sensorKind === 'temperature'){
+			for (var key in recibidos[id].values){
+				var a = moment(recibidos[id].values[key].x);
+				var b = moment();
+				if(a.diff(b, 'days') == 0){
+					if(max < recibidos[id].values[key].y){
+						max = recibidos[id].values[key].y;
+					}
+					if(min > recibidos[id].values[key].y){
+						min = recibidos[id].values[key].y;
+					}
+				}
+			}
+		}
+	}
+	return [max,min];
+};
 // Get Initial Data
 
 MongoClient.connect(mongoUrl, function(err, db) {
@@ -61,71 +101,292 @@ MongoClient.connect(mongoUrl, function(err, db) {
 		var lastDates = {
 			interior: 0,
 			exterior: 0,
-		}
-		db.collection('interior').find({ $where: function () { return Date.now() - this._id.getTimestamp() < (24 * 60 * 60 * 1000 * 7)  }  }).each(function(err,doc){
+			piscina: 0,
+			tinaja: 0,
+		};
+		db.collection('interior').find({ date: { $gte: new Date(moment().subtract(7, 'days')) } }).each(function(err,doc){
 			// Get all the saves and storage one register per hour
 			if(doc){
-				date = new Date(doc['date']).getTime();
-				if(verifyDate(lastDates.interior, date, 60 * 60 * 1000) ){
+				date = new Date(doc.date).getTime();
+				if(verifyDate(lastDates.interior, date, timeBetweenGraph * 1000) ){
 					lastDates.interior = date;
 
 					recibidos[0].values.push({
 						"x": date,
-						"y": Number(doc['temperature'])
+						"y": Number(doc.temperature)
 					});
 					recibidos[1].values.push({
 						"x": date,
-						"y": Number(doc['humidity'])
+						"y": Number(doc.humidity)
 					});
 				}
 			}
 		});
-		db.collection('exterior').find({ $where: function () { return Date.now() - this._id.getTimestamp() < (24 * 60 * 60 * 1000 * 7)  }  }).each(function(err,doc){
+		db.collection('exterior').find({ date: { $gte: new Date(moment().subtract(7, 'days')) } }).each(function(err,doc){
 			if(doc){
 				// Get all the saves and storage one register per hour
-				date = new Date(doc['date']).getTime();
-				if(verifyDate(lastDates.exterior, date, 60 * 60 * 1000) ){
+				date = new Date(doc.date).getTime();
+				if(verifyDate(lastDates.exterior, date, timeBetweenGraph * 1000) ){
 					lastDates.exterior = date;
 
 					recibidos[2].values.push({
 						"x": date,
-						"y": Number(doc['temperature'])
+						"y": Number(doc.temperature)
 					});
 					recibidos[3].values.push({
 						"x": date,
-						"y": Number(doc['humidity'])
+						"y": Number(doc.humidity)
+					});
+				}
+			}
+		});
+		db.collection('piscina').find({ date: { $gte: new Date(moment().subtract(7, 'days')) } }).each(function(err,doc){
+			if(doc){
+				// Get all the saves and storage one register per hour
+				date = new Date(doc.date).getTime();
+				if(verifyDate(lastDates.piscina, date, timeBetweenGraph * 1000) ){
+					lastDates.piscina = date;
+
+					recibidos[4].values.push({
+						"x": date,
+						"y": Number(doc.temperature)
+					});
+				}
+			}
+		});
+		db.collection('tinaja').find({ date: { $gte: new Date(moment().subtract(7, 'days')) } }).each(function(err,doc){
+			if(doc){
+				// Get all the saves and storage one register per hour
+				date = new Date(doc.date).getTime();
+				if(verifyDate(lastDates.tinaja, date, timeBetweenGraph * 1000) ){
+					lastDates.tinaja = date;
+					recibidos[5].values.push({
+						"x": date,
+						"y": Number(doc.temperature)
 					});
 				}
 			}
 		});
 	}
 	else{
-		console.log(err);
+		//console.log(err);
 		fn(0,'Cant connect to db');
 	}
-	db.close();
 });
 
 
 // Routers
-app.get("/pebble", function(req, res){
+// app.get("/pebble", function(req, res){
+// 	res.setHeader('Content-Type', 'application/json');
+// 	//console.log( recibidos[0].values[recibidos[0].values.length-1] );
+// 	json = {
+// 		'Interior': {
+// 			'Temperatura': recibidos[0].values[recibidos[0].values.length-1].y,
+// 			'Humedad': recibidos[1].values[recibidos[1].values.length-1].y
+// 		},
+// 		'Exterior':{
+// 			'Temperatura': recibidos[2].values[recibidos[2].values.length-1].y,
+// 			'Humedad': recibidos[3].values[recibidos[3].values.length-1].y,
+// 		}
+// 	}
+// 	res.send(JSON.stringify(json));
+// });
+
+app.post("/api/updateTemp", function(req, res){
 	res.setHeader('Content-Type', 'application/json');
-	console.log( recibidos[0].values[recibidos[0].values.length-1] );
-	json = {
-		'Interior': {
-			'Temperatura': recibidos[0].values[recibidos[0].values.length-1].y,
-			'Humedad': recibidos[1].values[recibidos[1].values.length-1].y
-		},
-		'Exterior':{
-			'Temperatura': recibidos[2].values[recibidos[2].values.length-1].y,
-			'Humedad': recibidos[3].values[recibidos[3].values.length-1].y,
+	var data;
+	if(req.body.tipo === 'aire'){
+		if(recibidos[0].values && recibidos[1].values && recibidos[2].values && recibidos[3].values){
+			var maxMinInterior = getMaxMin('Interior', new Date());
+			var maxMinExterior = getMaxMin('Exterior', new Date());
+			data = {
+				interior:{
+					temperature: recibidos[0].values[(recibidos[0].values.length-1)].y,
+					humidity: recibidos[1].values[(recibidos[1].values.length-1)].y,
+					'max-daily': maxMinInterior[0],
+					'min-daily': maxMinInterior[1]
+				},
+				exterior:{
+					temperature: recibidos[2].values[(recibidos[2].values.length-1)].y,
+					humidity: recibidos[3].values[(recibidos[3].values.length-1)].y,
+					'max-daily': maxMinExterior[0],
+					'min-daily': maxMinExterior[1]
+				}
+			};
 		}
+		else{
+			data = {
+				interior:{
+					temperature: 0,
+					humidity: 0,
+					'max-daily': 0, 
+					'min-daily': 0, 				
+				},
+				exterior:{
+					temperature: 0,
+					humidity: 0,
+					'max-daily': 0, 
+					'min-daily': 0, 				
+				}
+			};
+		}
+		
 	}
+	if(req.body.tipo === 'agua'){
+		if(recibidos[4].values.length > 0 && recibidos[5].values.length > 0){
+			data = {
+				piscina:{
+					temperature: recibidos[4].values[(recibidos[4].values.length-1)].y,
+				},
+				tinaja:{
+					temperature: recibidos[5].values[(recibidos[5].values.length-1)].y,
+				}
+			};
+		}
+		else{
+			data = {
+				piscina: { temperature: 0},
+				tinaja: { temperature: 0}
+			}
+		}
+		
+	}
+	res.send(JSON.stringify(data));
+});
+app.post("/api/getGraph", function(req, res){
+	function reduceHour(array){
+		// arrray = [
+		// {
+
+		// }]
+		// for()
+	}
+	function reduceDay(array){
+
+	}
+	function reduceMonth(array){
+
+	}
+	function reduceYear(array){
+
+	}
+
+
+	
+	var resultados = {},toSend = {};
+	MongoClient.connect(mongoUrl, function(err, db) { if(!err){
+
+		// Get Data
+		if(req.body.type === 'air'){
+			db.collection('interior').find({ date: { $gte: new Date(req.body.start) } }).each(function(err,doc){
+				var moment = moment(doc.date);
+				var hora   = moment.get('hour');
+				var dia    = moment.get('day');
+				var mes    = moment.get('month');
+				var anio   = moment.get('year');
+				
+				if(req.body.of == 'temperatura'){
+					resultados['interior'][anio][mes][dia][hora].append(Number(doc.temperature));
+				}
+				else{
+					resultados['interior'][anio][mes][dia][hora].append(Number(doc.humidity));
+				}
+			});
+			db.collection('exterior').find({ date: { $gte: new Date(req.body.start) } }).each(function(err,doc){
+				var moment = moment(doc.date);
+				var moment = moment(doc.date);
+				var hora   = moment.get('hour');
+				var dia    = moment.get('day');
+				var mes    = moment.get('month');
+				var anio   = moment.get('year');
+
+				if(req.body.of == 'temperatura'){
+					resultados['exterior'][anio][mes][dia][hora].append(Number(doc.temperature));
+				}
+				else{
+					resultados['exterior'][anio][mes][dia][hora].append(Number(doc.humidity));
+				}
+			});
+		}
+		if(req.body.type == 'water'){
+			db.collection('piscina').find({ date: { $gte: new Date(req.body.start) } }).each(function(err,doc){
+				var moment = moment(doc.date);
+				var moment = moment(doc.date);
+				var hora   = moment.get('hour');
+				var dia    = moment.get('day');
+				var mes    = moment.get('month');
+				var anio   = moment.get('year');
+
+				if(req.body.of == 'temperatura'){
+					resultados['piscina'][anio][mes][dia][hora].append(Number(doc.temperature));
+				}
+				else{
+					resultados['piscina'][anio][mes][dia][hora].append(Number(doc.humidity));
+				}
+			});
+			db.collection('tinaja').find({ date: { $gte: new Date(req.body.start) } }).each(function(err,doc){
+				var moment = moment(doc.date);
+				var moment = moment(doc.date);
+				var hora   = moment.get('hour');
+				var dia    = moment.get('day');
+				var mes    = moment.get('month');
+				var anio   = moment.get('year');
+
+				if(req.body.of == 'temperatura'){
+					resultados['tinaja'][anio][mes][dia][hora].append(Number(doc.temperature));
+				}
+				else{
+					resultados['tinaja'][anio][mes][dia][hora].append(Number(doc.humidity));
+				}
+			});
+		} 
+	} });
+
+	function dtos(date){
+		var fecha = 'Date('+moment(date).subtract(1, 'months').format("YYYY, M, D, HH, mm, ss")+')';
+		return fecha;
+	}
+	res.setHeader('Content-Type', 'application/json');
+	var date = new Date();
+	date.setDate(date.getDate() - 10);
+	var type = 'humedad';
+	json = {
+		'type': type,
+		data: 
+		{
+			cols: [
+				{ label:"Fecha", type:"date" },
+				{ label:"Promedio "+type+" Dia Interior", type:"number" },
+				{ label:"Promedio "+type+" Noche Interior", type:"number" },
+				{ label:"Promedio "+type+" Dia Exterior", type:"number" },
+				{ label:"Promedio "+type+" Noche Exterior", type:"number" }
+			],
+			rows: [
+				{c: [
+						{v:dtos(new Date())},
+						{v:50},
+						{v:36},
+						{v:25},
+						{v:30}
+					]
+				},
+				{c: [
+						{v:dtos(date)},
+						{v:48},
+						{v:34},
+						{v:23},
+						{v:28}
+					]
+				}
+			]
+		}
+	};
+	// console.log(json);
 	res.send(JSON.stringify(json));
 });
 
-app.get("/", function(req, res){
-	res.sendFile(rootDir+"home.html");
+app.get("*", function(req, res){
+	res.sendFile(rootDir+"index.html");
 });
 
 
@@ -133,15 +394,8 @@ app.get("/", function(req, res){
 
 io.sockets.on('connection', function (socket) {
 	var clientIp = socket.request.connection._peername.address;
-	console.log('Client IP ('+clientIp+') Connected');
-	// Return historical on connect
+	//console.log('Client IP ('+clientIp+') Connected');
 	
-	socket.on('getHistoricalTemperature',function(fn){
-		console.log("Send Historical");
-		if(typeof fn == 'function'){
-			fn(recibidos,'');
-		}
-	})
 	//  Update Temperature (To Raspberry PI)
 	socket.on('updateLast', function(){
 		io.emit('updateLast');
@@ -149,60 +403,124 @@ io.sockets.on('connection', function (socket) {
 
 	// Process Data from Raspberry PI
 	socket.on('updateDb', function (data, fn) {
-		console.log("Trying to Update Log");
+		//console.log("Trying to Update Log");
 		date = new Date().getTime();
 		// Verify last Update 
 		if( verifyDate( lastUpdates[data.tipo], date, timeBetweenUpdates * 1000 ) ){
-			console.log("Updating Log");
-			if(data.token == token){ 
+			//console.log("Updating Log");
+			if(data.token === token){ 
 				// Store data on DB
 				MongoClient.connect(mongoUrl, function(err, db) {
 				  if(!err) {
-					db.collection(data.tipo).insertOne(
-					   {
-							date: new Date(),
-							temperature: data.values.temperature,
-							humidity: data.values.humidity,
-					   }
-					)
+				  	if(data.values.humidity){
+				  		db.collection(data.tipo).insertOne(
+						   {
+								date: new Date(),
+								temperature: Number(data.values.temperature),
+								humidity: Number(data.values.humidity),
+						   }
+						);
+				  	}
+				  	else{
+				  		db.collection(data.tipo).insertOne(
+						   {
+								date: new Date(),
+								temperature: Number(data.values.temperature),
+						   }
+						);
+				  	}
+					
+					db.close();
 					fn(1,'Updated');
 				  }
 				  else{
-					console.log(err)
+					//console.log(err)
 					fn(0,'Cant connect to db');
 				  }
-				  db.close();
 				});
 
-				// Send Data to the connected Users
-				var postData = {
-					tipo: data.tipo,
-					values: data.values,
-				}
-				io.emit('updateData', postData);
-
 				// Store Data on our app
-				if(data.tipo == "interior"){
-					lastUpdates.interior = date;
-					recibidos[0].values.push({
-						"x": date,
-						"y": Number(data.values.temperature)
-					});
-					recibidos[1].values.push({
-						"x": date,
-						"y": Number(data.values.humidity)
-					});
+				if(data.tipo === "interior"){
+					if(verifyDate(lastUpdates.interior, date, timeBetweenGraph * 1000) ){
+						lastUpdates.interior = date;
+						recibidos[0].values.push({
+							"x": date,
+							"y": Number(data.values.temperature)
+						});
+						recibidos[1].values.push({
+							"x": date,
+							"y": Number(data.values.humidity)
+						});
+
+						// Send Data to the connected Users
+						maxMin = getMaxMin('Interior', new Date());
+						postData = {
+							"interior":{
+								temperature: recibidos[0].values[(recibidos[0].values.length-1)].y,
+								humidity: recibidos[1].values[(recibidos[1].values.length-1)].y,
+								'max-daily': maxMin[0],
+								'min-daily': maxMin[1]
+
+							}
+						};
+						io.emit('updateData', postData);
+					}
 				}
-				if(data.tipo == "exterior"){
-					lastUpdates.exterior = date;
-					recibidos[2].values.push({
-						"x": date,
-						"y": Number(data.values.temperature)
-					});
-					recibidos[3].values.push({
-						"x": date,
-						"y": Number(data.values.humidity)
-					});
+				if(data.tipo === "exterior"){
+					if(verifyDate(lastUpdates.exterior, date, timeBetweenGraph * 1000) ){
+						lastUpdates.exterior = date;
+						recibidos[2].values.push({
+							"x": date,
+							"y": Number(data.values.temperature)
+						});
+						recibidos[3].values.push({
+							"x": date,
+							"y": Number(data.values.humidity)
+						});
+						// Send Data to the connected Users
+						maxMin = getMaxMin('Exterior', new Date());
+						postData = {
+							"exterior":{
+								temperature: recibidos[2].values[(recibidos[2].values.length-1)].y,
+								humidity: recibidos[3].values[(recibidos[3].values.length-1)].y,
+								'max-daily': maxMin[0],
+								'min-daily': maxMin[1]
+							}
+						};
+						io.emit('updateData', postData);
+					}
+				}
+				if(data.tipo === "piscina"){
+					if(verifyDate(lastUpdates.piscina, date, timeBetweenGraph * 1000) ){
+						recibidos[4].values.push({
+							"x": date,
+							"y": Number(data.values.temperature)
+						});
+						lastUpdates.piscina = date;
+						// Send Data to the connected Users
+						postData = {
+							"piscina":{
+								temperature: recibidos[4].values[(recibidos[4].values.length-1)].y,
+							}
+						};
+						io.emit('updateData', postData);
+					}
+				}
+				if(data.tipo === "tinaja"){
+					if(verifyDate(lastUpdates.tinaja, date, timeBetweenGraph * 1000) ){
+						recibidos[5].values.push({
+							"x": date,
+							"y": Number(data.values.temperature)
+						});
+						lastUpdates.tinaja = date;
+						// Send Data to the connected Users
+						postData = {
+							"tinaja":{
+								temperature: recibidos[5].values[(recibidos[5].values.length-1)].y,
+							}
+						};
+						io.emit('updateData', postData);
+					}
 				}
 
 			}
@@ -211,38 +529,64 @@ io.sockets.on('connection', function (socket) {
 			}
 		}
 		else{
-			console.log('Getting Temperature Info from Cache');
-			if(recibidos[0].values.length > 1){
-				if(data.tipo == 'interior'){
+			//console.log('Getting Temperature Info from Cache');
+			if(recibidos[0].values.length > 0){
+				if(data.tipo === 'interior'){
+					maxMin = getMaxMin('Interior', new Date());
 					postData = {
-						tipo: "interior",
-						values: {
-							temperature: recibidos[0].values[(recibidos[0].values.length-1)]['y'],
-							humidity: recibidos[1].values[(recibidos[1].values.length-1)]['y'],
+						"interior":{
+							temperature: recibidos[0].values[(recibidos[0].values.length-1)].y,
+							humidity: recibidos[1].values[(recibidos[1].values.length-1)].y,
+							'max-daily': maxMin[0],
+							'min-daily': maxMin[1]
 						}
-					}
+					};
+					io.emit('updateData', postData);
 				}
-				else if(data.tipo == 'exterior'){
-					postData = {tipo: "exterior",
-						values: {
-							temperature: recibidos[2].values[(recibidos[2].values.length-1)]['y'],
-							humidity: recibidos[3].values[(recibidos[3].values.length-1)]['y'],
-						}
-					}
-				}
-				io.emit('updateData', postData);
 			}
-			else{
-				console.log('Looks like we dont have registers of temperature',recibidos[0].values);
+			if(recibidos[2].values.length > 0){
+				if(data.tipo === 'exterior'){
+					maxMin = getMaxMin('Exterior', new Date());
+					postData = {
+						"exterior":{
+							temperature: recibidos[2].values[(recibidos[2].values.length-1)].y,
+							humidity: recibidos[3].values[(recibidos[3].values.length-1)].y,
+							'max-daily': maxMin[0],
+							'min-daily': maxMin[1]
+
+						}
+					};
+					io.emit('updateData', postData);
+				}
+				
+			}
+			if(recibidos[4].values.length > 0){
+				if(data.tipo === 'Piscina'){
+					postData = {
+						"piscina":{
+							temperature: recibidos[4].values[(recibidos[4].values.length-1)].y,
+
+						}
+					};
+					io.emit('updateData', postData);
+				}
+				
+			}
+			if(recibidos[5].values.length > 0){
+				if(data.tipo === 'Tinaja'){
+					postData = {
+						"tinaja":{
+							temperature: recibidos[5].values[(recibidos[5].values.length-1)].y,
+
+						}
+					};
+					io.emit('updateData', postData);
+				}
+				
 			}
 		} 
 	});
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
-	});
-
-	// Try to update data when user enter
-	io.emit('updateLast');
+	
 });
 
 console.log("Listening on port " + port);
